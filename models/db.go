040a7e732c0045cff5504dfef76ce62cc994650c
Keriteal <keriteal985@gmail.com>
2020-08-15 10:59:43 +0800

    add

---
+++ models/db.go
@@ -0,0 +1,42 @@
+package models
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	"github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+type DB struct {
+	DB               *gorm.DB
+	dbType           string
+	dbConnectionPath string
+}
+
+func NewDB(dpType, dbConnectionPath string) *DB {
+	return &DB{
+		dbType:           dpType,
+		dbConnectionPath: dbConnectionPath,
+	}
+}
+
+func (db *DB) Connect() error {
+	var dbConnection *gorm.DB
+	var err error
+	if db.dbType == "sqlite3" {
+		dbConnection, err = gorm.Open(sqlite.Open(db.dbConnectionPath), &gorm.Config{})
+	} else if db.dbType == "postgres" {
+		dbConnection, err = gorm.Open(postgres.Open(db.dbConnectionPath), &gorm.Config{})
+	} else {
+		err = errors.New("invalid dbtype")
+	}
+	if err != nil {
+		return fmt.Errorf("failed to initialize database, got error: %v", err)
+	}
+	dbConnection.AutoMigrate(&Mpc{})
+	//dbConnection.LogMode(true)
+	db.DB = dbConnection
+	return err
+}
